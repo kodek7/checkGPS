82ed07423a1c9e1b7a5a4dc945bf2a05cfd37de8
Sperc <pawels69@gmail.com>
2016-11-25 00:00:19 +0100

    add Service method

--- app/src/main/java/com/example/grzeg/checkgps/MainActivity.java
+++ app/src/main/java/com/example/grzeg/checkgps/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.grzeg.checkgps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.grzeg.checkgps.api.ApiEngine;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_start = (Button)findViewById(R.id.btn_ON);
+        Button btn_stop = (Button)findViewById(R.id.btn_OFF);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService();
+            }
+        });
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService();
+            }
+        });
+    }
+    public void startService()
+    {
+        Intent intent = new Intent(this,ApiEngine.class);
+        startService(intent);
+    }
+    public void stopService()
+    {
+        Intent intent = new Intent(this,ApiEngine.class);
+        stopService(intent);
     }
 }
--- app/src/main/java/com/example/grzeg/checkgps/api/ApiEngine.java
+++ app/src/main/java/com/example/grzeg/checkgps/api/ApiEngine.java
@@ -1,8 +1,37 @@
 package com.example.grzeg.checkgps.api;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Toast;
+
 /**
  * Created by grzeg on 24.11.2016.
  */
 
-public class ApiEngine {
+public class ApiEngine extends Service{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this,"service started",Toast.LENGTH_SHORT).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this,"service stop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
